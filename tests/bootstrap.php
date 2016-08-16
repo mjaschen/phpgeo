5616f49d1a867cf826f1ebcba435fbf8c10f2ebd
Marcus Jaschen <mjaschen@gmail.com>
2016-08-16 22:41:06 +0200

    chore: remove autoloader from PHPUnit bootstrap, use Composer's autoloader instead

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,16 +1,3 @@
 <?php
-/**
- * Simple PSR-0 Autoloader
- *
- * @category  Location
- * @author    Marcus T. Jaschen <mjaschen@gmail.com>
- */
-spl_autoload_register(
-    function ($class) {
-        $file = __DIR__ . '/../src/' . strtr($class, '\\', '/') . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-            return true;
-        }
-    }
-);
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
