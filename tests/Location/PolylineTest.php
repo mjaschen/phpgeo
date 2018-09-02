0c8ddd42859c33899e87a84acffc52b6ff39e8e2
Clement G <contact@clemdesign.fr>
2018-09-02 21:26:04 +0200

    Add test to new behavior of Polyline

--- tests/Location/PolylineTest.php
+++ tests/Location/PolylineTest.php
@@ -9,81 +9,92 @@ use PHPUnit\Framework\TestCase;
 
 class PolylineTest extends TestCase
 {
-    /**
-     * @var \Location\Polyline
-     */
-    protected $polyline;
+  /**
+   * @var \Location\Polyline
+   */
+  protected $polyline;
 
-    public function setUp()
-    {
-        $this->polyline = new Polyline();
-        $this->polyline->addPoint(new Coordinate(52.5, 13.5));
-        $this->polyline->addPoint(new Coordinate(64.1, - 21.9));
-        $this->polyline->addPoint(new Coordinate(40.7, - 74.0));
-        $this->polyline->addPoint(new Coordinate(33.9, - 118.4));
-    }
+  public function setUp()
+  {
+    $this->polyline = new Polyline();
+    $this->polyline->addPoint(new Coordinate(52.5, 13.5));
+    $this->polyline->addPoint(new Coordinate(64.1, -21.9));
+    $this->polyline->addPoint(new Coordinate(40.7, -74.0));
+    $this->polyline->addPoint(new Coordinate(33.9, -118.4));
+  }
 
-    public function testCreatePolyline()
-    {
-        $this->assertCount(4, $this->polyline->getPoints());
-    }
+  public function testCreatePolyline()
+  {
+    $this->assertCount(4, $this->polyline->getPoints());
+  }
 
-    public function testGetSegments()
-    {
-        $segments = $this->polyline->getSegments();
+  public function testGetSegments()
+  {
+    $segments = $this->polyline->getSegments();
 
-        $this->assertEquals(new Line(new Coordinate(52.5, 13.5), new Coordinate(64.1, - 21.9)), $segments[0]);
-        $this->assertEquals(new Line(new Coordinate(64.1, - 21.9), new Coordinate(40.7, - 74.0)), $segments[1]);
-        $this->assertEquals(new Line(new Coordinate(40.7, - 74.0), new Coordinate(33.9, - 118.4)), $segments[2]);
-    }
+    $this->assertEquals(new Line(new Coordinate(52.5, 13.5), new Coordinate(64.1, -21.9)), $segments[0]);
+    $this->assertEquals(new Line(new Coordinate(64.1, -21.9), new Coordinate(40.7, -74.0)), $segments[1]);
+    $this->assertEquals(new Line(new Coordinate(40.7, -74.0), new Coordinate(33.9, -118.4)), $segments[2]);
+  }
 
-    public function testGetSegmentsForOnlyOnePointInLineWorksAsExpected()
-    {
-        $polyline = new Polyline();
-        $polyline->addPoint(new Coordinate(52.5, 13.5));
+  public function testGetSegmentsForOnlyOnePointInLineWorksAsExpected()
+  {
+    $polyline = new Polyline();
+    $polyline->addPoint(new Coordinate(52.5, 13.5));
 
-        $this->assertEquals([], $polyline->getSegments());
-    }
+    $this->assertEquals([], $polyline->getSegments());
+  }
 
-    public function testGetLength()
-    {
-        $this->assertEquals(10576798.9, $this->polyline->getLength(new Vincenty()), '', 0.1);
-    }
+  public function testGetLength()
+  {
+    $this->assertEquals(10576798.9, $this->polyline->getLength(new Vincenty()), '', 0.1);
+  }
 
-    public function testGetReverseWorksAsExpected()
-    {
-        $reversed = $this->polyline->getReverse();
+  public function testGetReverseWorksAsExpected()
+  {
+    $reversed = $this->polyline->getReverse();
 
-        $expected = new Polyline();
-        $expected->addPoint(new Coordinate(33.9, - 118.4));
-        $expected->addPoint(new Coordinate(40.7, - 74.0));
-        $expected->addPoint(new Coordinate(64.1, - 21.9));
-        $expected->addPoint(new Coordinate(52.5, 13.5));
+    $expected = new Polyline();
+    $expected->addPoint(new Coordinate(33.9, -118.4));
+    $expected->addPoint(new Coordinate(40.7, -74.0));
+    $expected->addPoint(new Coordinate(64.1, -21.9));
+    $expected->addPoint(new Coordinate(52.5, 13.5));
 
-        $this->assertEquals($expected, $reversed);
-    }
+    $this->assertEquals($expected, $reversed);
+  }
 
-    public function testReverseTwiceWorksAsExpected()
-    {
-        $doubleReversed = $this->polyline->getReverse()->getReverse();
+  public function testReverseTwiceWorksAsExpected()
+  {
+    $doubleReversed = $this->polyline->getReverse()->getReverse();
 
-        $this->assertEquals($this->polyline, $doubleReversed);
-    }
+    $this->assertEquals($this->polyline, $doubleReversed);
+  }
 
-    public function testAddSinglePointWorksAsExpeted() {
-      $expected = $this->polyline;
+  public function testAddSinglePointWorksAsExpeted()
+  {
+    $expected = $this->polyline;
 
-      $expected->addSinglePoint(new Coordinate(52.5, 13.5));
-      $expected->addSinglePoint(new Coordinate(64.1, - 21.9));
-      $expected->addSinglePoint(new Coordinate(40.7, - 74.0));
-      $expected->addSinglePoint(new Coordinate(33.9, - 118.4));
+    $expected->addSinglePoint(new Coordinate(52.5, 13.5));
+    $expected->addSinglePoint(new Coordinate(64.1, -21.9));
+    $expected->addSinglePoint(new Coordinate(40.7, -74.0));
+    $expected->addSinglePoint(new Coordinate(33.9, -118.4));
 
-      $this->assertEquals($this->polyline, $expected);
-    }
+    $this->assertEquals($this->polyline, $expected);
+  }
 
-    public function testgetMiddlePointWorksAsExpected() {
-      $middle = $this->polyline->getMiddlePoint();
+  public function testGetMiddlePointWorksAsExpected()
+  {
+    $middle = $this->polyline->getMiddlePoint();
 
-      $this->assertEquals($middle, new Coordinate(47.8, -50.2));
-    }
+    $this->assertEquals($middle, new Coordinate(47.8, -50.2));
+  }
+
+  public function testGetMiddlePointWithNoPoints()
+  {
+    $polyline = new Polyline();
+
+    $middle = $polyline->getMiddlePoint();
+
+    $this->assertEquals($middle, null);
+  }
 }
