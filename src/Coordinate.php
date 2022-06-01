9eeb8c948486aaff6f841b3bf8475f38c9ff1bfc
Marcus Jaschen <mjaschen@gmail.com>
2022-06-01 08:24:31 +0200

    remove `equals()` and use `hasSameLocation()`; use `instanceof` instead `is_a()`

--- src/Coordinate.php
+++ src/Coordinate.php
@@ -124,24 +124,13 @@ class Coordinate implements GeometryInterface
     /**
      * Checks if this point intersects a given geometry.
      */
-    public function intersects(
-        GeometryInterface $geometry,
-        bool $precise = false
-    ): bool {
-        if (is_a($geometry, 'Location\Coordinate')) {
-            return $this->equals($geometry);
+    public function intersects(GeometryInterface $geometry): bool
+    {
+        if ($geometry instanceof self) {
+            return $this->hasSameLocation($geometry);
         }
 
         return $geometry->contains($this);
-    }
-
-    /**
-     * Checks if two coordinates are equal.
-     */
-    public function equals(Coordinate $coordinate)
-    {
-        return $coordinate->getLng() === $this->lng &&
-            $coordinate->getLat() === $this->lat;
     }
 
     public function format(FormatterInterface $formatter): string
