b0ccfc0c668591d9e44212f9f806befc8487cab4
Marcus Jaschen <mjaschen@gmail.com>
2023-12-03 13:49:44 +0100

    remove obsolete doc blocks; throw JsonException when encoding fails

--- src/Formatter/Polyline/GeoJSON.php
+++ src/Formatter/Polyline/GeoJSON.php
@@ -6,18 +6,8 @@ namespace Location\Formatter\Polyline;
 
 use Location\Polyline;
 
-/**
- * GeoJSON Polyline Formatter
- *
- * @author Richard Barnes <rbarnes@umn.edu>
- */
 class GeoJSON implements FormatterInterface
 {
-    /**
-     * @param Polyline $polyline
-     *
-     * @return string
-     */
     public function format(Polyline $polyline): string
     {
         $points = [];
@@ -30,7 +20,8 @@ class GeoJSON implements FormatterInterface
             [
                 'type'        => 'LineString',
                 'coordinates' => $points,
-            ]
+            ],
+            JSON_THROW_ON_ERROR
         );
     }
 }
