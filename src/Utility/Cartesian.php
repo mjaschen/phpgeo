bce5224f37a90e633748af6e13825b68d6762c89
Marcus Jaschen <mjaschen@gmail.com>
2023-12-06 15:56:23 +0100

    add public properties; deprecate getters; update doc blocks

--- src/Utility/Cartesian.php
+++ src/Utility/Cartesian.php
@@ -6,19 +6,12 @@ namespace Location\Utility;
 
 class Cartesian
 {
-    /**
-     * Cartesian constructor.
-     *
-     * @param $x
-     * @param $y
-     * @param $z
-     */
-    public function __construct(private readonly float $x, private readonly float $y, private readonly float $z)
+    public function __construct(public readonly float $x, public readonly float $y, public readonly float $z)
     {
     }
 
     /**
-     * @return float
+     * @deprecated Use property instead
      */
     public function getX(): float
     {
@@ -26,7 +19,7 @@ class Cartesian
     }
 
     /**
-     * @return float
+     * @deprecated Use property instead
      */
     public function getY(): float
     {
@@ -34,16 +27,13 @@ class Cartesian
     }
 
     /**
-     * @return float
+     * @deprecated Use property instead
      */
     public function getZ(): float
     {
         return $this->z;
     }
 
-    /**
-     * @return Cartesian
-     */
     public function add(Cartesian $other): Cartesian
     {
         return new self(
