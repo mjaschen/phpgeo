7d3a5b1a22c08999fe93cc72a94bb801495fd522
Marcus Jaschen <mail@marcusjaschen.de>
2025-12-02 12:40:03 +0100

    improve checks for intersecting lines when collinear points are involved
    fixes #96

--- tests/Regression/Github/96/Issue96Test.php
+++ tests/Regression/Github/96/Issue96Test.php
@@ -8,26 +8,29 @@ use PHPUnit\Framework\TestCase;
 
 class Issue96Test extends TestCase
 {
-    public function testForIssue96(): void
+    public function testFailingForIssue96(): void
     {
         $line1 = new Line(
             new Coordinate(0.0, 0.0),
-            new Coordinate(0.0, 2.0)
+            new Coordinate(0.0, 2.0),
         );
         $line2 = new Line(
             new Coordinate(2.0, 2.0),
-            new Coordinate(0.0, 10.0)
+            new Coordinate(0.0, 10.0),
         );
 
-        //$this->assertFalse($line1->intersectsLine($line2));
+        $this->assertFalse($line1->intersectsLine($line2));
+    }
 
+    public function testPassingForIssue96(): void
+    {
         $line1 = new Line(
             new Coordinate(0.0, 0.0),
-            new Coordinate(0.0, 1.999999)
+            new Coordinate(0.0, 1.999999),
         );
         $line2 = new Line(
             new Coordinate(2.0, 2.0),
-            new Coordinate(0.0, 10.0)
+            new Coordinate(0.0, 10.0),
         );
 
         $this->assertFalse($line1->intersectsLine($line2));
